a4917fb65051b7066b29366f4f0f40ad7b7aa676
Mohit Kumar <itsmohitmk99@gmail.com>
2021-05-24 01:45:31 +0530

    url

--- buttonpyhton/settings.py
+++ buttonpyhton/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+b3yn!%z_4r7&1c09@!w29oe*boay19bjf3%kf3_!m@iu_grx8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1' , 'https://young-springs-70575.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1' , 'link-directlt-mohit.herokuapp.com']
 
 
 # Application definition
